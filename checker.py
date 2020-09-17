cf379a348200aec3b5225814c97953921973b60b
ShahinSha-dot <64163306+ShahinSha-dot@users.noreply.github.com>
2020-09-17 12:59:47 +0530

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,35 @@
+class Disease:
+     def __init__(self, name, symptoms):
+         self.name = name
+         self.symptoms = symptoms
+
+
+known_diseases = [
+   Disease('cold', set(
+     "sore throat|runny nose|congestion|cough|aches".split("|"))
+     ),
+   Disease('flu', set(
+     "fever|headache|muscle aches|returning fever".split("|"))
+     ),
+   Disease('ebola', set(
+     "tiredness|death|bruising|black blood".split("|"))
+     ),
+   ]
+
+
+# note: for Python 2, use "raw_input" instead of input
+symptoms = input("Please enter symptoms separated by commas: ")
+symptoms = symptoms.lower()
+symptoms = symptoms.split(",")
+
+possible = []
+for symptom in symptoms:
+     for disease in known_diseases:
+         if symptom in disease.symptoms:
+             possible.append(disease.name)
+
+if possible:
+     print("You may have these diseases:")
+     print(*possible)
+else:
+     print("Good news! You're going to have a disease named after you!")
