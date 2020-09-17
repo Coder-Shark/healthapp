a8b00124348b160d0f04511ea9955868bf383915
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-09-18 01:13:38 +0530

    Create wiki.py

---
+++ wiki.py
@@ -0,0 +1,23 @@
+import requests as r
+from bs4 import BeautifulSoup
+import html5lib
+r=r.get('https://en.wikipedia.org/wiki/epilepsy')
+so=BeautifulSoup(r.content,'html5lib')
+title=so.find("h1",class_="firstHeading").getText()
+p=so.find_all("p")[1].getText()
+s=so.find('table',class_="infobox")
+f=s.find('tbody')
+one=s.find_all('tr')[1]
+oneo=one.find('th').getText()
+onet=one.find('td').getText()
+two=s.find_all('tr')[5]
+twoo=two.find('th').getText()
+twot=two.find('td').getText()
+three=s.find_all('tr')[6]
+threeo=three.find('th').getText()
+threet=three.find('td').getText()
+four=s.find_all('tr')[7]
+fouro=four.find('th').getText()
+fourt=four.find('td').getText()
+
+
