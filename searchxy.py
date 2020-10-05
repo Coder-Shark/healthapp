1bfbd74f200bc3b7486a58eab14cea6d93d75ac7
Shahin Sha <shahin@pop-os.localdomain>
2020-10-06 00:34:40 +0530

    updated searchxy.py

--- searchxy.py
+++ searchxy.py
@@ -8,15 +8,18 @@ from bs4 import BeautifulSoup
 import html5lib
 import lxml
 import time
+
+root= tk.Tk()
+
 def timte():
     time_now=time.strftime('%r:%x')
     timestring.config(text=time_now)
     timestring.after(100,timte)
 timestring=tk.Label(root)
 timestring.place(x=90,y=100)
-timte()  
+timte()
 
-root= tk.Tk()
+
 
 canvas1 = tk.Canvas(root, width = 400, height = 300,  relief = 'raised')
 canvas1.pack()
@@ -35,17 +38,17 @@ canvas1.create_window(200, 140, window=entry1)
 
 
 def getSR():
-    
+
     x1 = entry1.get()
-    
+
     label3 = tk.Label(root, text= 'The possible results of ' + x1 + ' is:',font=('century schoolbook', 15))
     canvas1.create_window(200, 210, window=label3)
-    
+
     label4 = tk.Label(root,font=('century', 12, 'bold'))
     canvas1.create_window(200, 230, window=label4)
-    
-    
-    
+
+
+
 button1 = tk.Button(text='Search',command=getSR, bg='brown', fg='white', font=('century schoolbook', 12 , 'bold'))
 canvas1.create_window(200, 180, window=button1)
 
